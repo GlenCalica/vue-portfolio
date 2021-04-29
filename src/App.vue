f89259f03181432d7aa202ee3c7360c5308ba315
Glen <glencalica@gmail.com>
2021-04-29 16:40:10 -0400

    added basic layouts

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+   <the-navbar></the-navbar>
+   <the-header></the-header>
+   <the-about></the-about>
+   <the-projects></the-projects>
+   <the-footer></the-footer>
+</template>
+
+<script>
+import TheNavbar from './layouts/TheNavbar';
+import TheHeader from './layouts/TheHeader';
+import TheAbout from './layouts/TheAbout';
+import TheProjects from './layouts/TheProjects';
+import TheFooter from './layouts/TheFooter';
+
+export default {
+   components: {
+      TheNavbar,
+      TheHeader,
+      TheAbout,
+      TheProjects,
+      TheFooter
+   }
+}
+</script>
