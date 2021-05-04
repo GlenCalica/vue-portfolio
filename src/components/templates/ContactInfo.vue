20b45ce0d4ee5c08e76224c810e94c994af46e96
Glen <glencalica@gmail.com>
2021-05-04 11:57:48 -0400

    contact card functionality and styling

--- src/components/templates/ContactInfo.vue
+++ src/components/templates/ContactInfo.vue
@@ -1,16 +1,18 @@
 <template>
-   <form action="">
-      <div class="flex">
-         <div class="w-1/2 mr-8">
-            <label for="name"><p>Name</p></label>
-            <input type="text" class="w-full">
+   <form action="mailto:glencalica@gmail.com" method="post" enctype="text/plain">
+      <div class="flex my-8">
+         <div class="w-1/2 mr-8 mb-4">
+            <label for="name"><p class="mb-2">Name</p></label>
+            <input type="text" name="name" class="w-full bg-custom-white border-b-4 border-custom-gray py-2 focus:outline-black" required>
          </div>
          <div class="w-1/2">
-            <label for="email"><p>Email</p></label>
-            <input type="text" class="w-full">
+            <label for="email"><p class="mb-2">Email</p></label>
+            <input type="text" name="email" class="w-full bg-custom-white border-b-4 border-custom-gray py-2 focus:outline-black" required>
          </div>
       </div>
-      <label for="message"><p>Message</p></label>
-      <textarea name="message" id="" cols="30" rows="10"></textarea>
+      <label for="message"><p class="mb-2">Message</p></label>
+      <textarea name="message" id="" cols="30" rows="3" class="w-full bg-custom-white border-b-4 border-custom-gray py-2 resize-none focus:outline-black" required></textarea>
+
+      <input type="submit" value="Get in Touch" class="p-4 border-custom-gray border-4 text-xl font-medium my-12">
    </form>
 </template>
