8bbde106aa54efc225d0cf1d9da0a070efdebf7e
Glen <glencalica@gmail.com>
2021-05-03 21:09:18 -0400

    added transition to animated cards

--- src/components/UI/AnimatedCard.vue
+++ src/components/UI/AnimatedCard.vue
@@ -1,8 +1,10 @@
 <template>
    <div>
       <div class="fixed h-screen w-screen top-0 left-0 z-30" @click="$emit('close')"></div>
+
       <base-card class="center fixed z-40">
          <img class="fixed right-16" :src="require('../../assets/icons/close.svg')" @click="$emit('close')">
+
          <slot></slot>
       </base-card>
    </div>
@@ -14,7 +16,7 @@ import BaseCard from './BaseCard.vue';
 export default {
    components: {
       BaseCard
-   },
+   },   
    emits: ['close']
 }
 </script>
--- src/components/layouts/TheNavbar.vue
+++ src/components/layouts/TheNavbar.vue
@@ -1,19 +1,14 @@
 <template>
-   <section class="container fixed flex justify-between py-24 z-10">
-      <h1 class="text-4xl font-bold p-3 bg-custom-gray">GC</h1>
-      <p class="text-xl font-bold self-center" @click="toggleCard">CONTACT</p>
-   </section>
+   <transition name="fade" mode="out-in">
+      <section class="container fixed flex justify-between py-24 z-10" v-if="!cardIsOpen">
+         <h1 class="text-4xl font-bold p-3 bg-custom-gray">GC</h1>
+         <p class="text-xl font-bold self-center" @click="toggleCard">CONTACT</p>
+      </section>
 
-
-   <transition name="fade">
-      <animated-card v-if="cardIsOpen" @close="toggleCard">
-         <!-- <p>Test</p> -->
+      <animated-card v-else @close="toggleCard">
+         <p>Test</p>
       </animated-card>
-      <!-- <p v-if="cardIsOpen" @close="toggleCard">
-         Test
-      </p> -->
    </transition>
-
 </template>
 
 <script>
@@ -30,15 +25,3 @@ export default {
    }
 }
 </script>
-
-<style scoped>
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .2s ease;
-}
-
-.fade-enter-from, .fade-leave-to {
-  opacity: 0;
-}
-
-</style>
