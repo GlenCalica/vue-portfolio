25e0d56eb60a7315bdd59104fefe734358a70df0
Glen <glencalica@gmail.com>
2021-05-05 15:54:02 -0400

    minor styling fixes
    -added button styling
    -moved css hover styling to tailwind

--- src/components/templates/ProjectDetails.vue
+++ src/components/templates/ProjectDetails.vue
@@ -8,8 +8,8 @@
    <p class="mt-4 text-2xl">Made with 
       <span class="font-bold">{{ tools }}</span>
    </p>
-   <p v-if="github" class="text-2xl my-4"><a :href="github" target="_blank" class="font-bold">Link</a> to Github repository</p>
-   <p v-if="liveExample" class="text-2xl"><a :href="liveExample" target="_blank" class="font-bold">Link</a> to live example</p>
+   <p v-if="github" class="text-2xl my-4"><a :href="github" target="_blank" class="font-bold hover:bg-custom-mint transition-colors">Link</a> to Github repository</p>
+   <p v-if="liveExample" class="text-2xl"><a :href="liveExample" target="_blank" class="font-bold hover:bg-custom-mint transition-colors">Link</a> to live example</p>
 </template>
 
 <script>
